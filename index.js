6d4ba4a9e13c078821886ed16b6af99515dfe680
Raraph84 <contact@raraph.fr>
2022-06-18 14:34:13 +0200

    Clean

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { default: Collection } = require("@discordjs/collection");
 const Fs = require("fs");
+const Config = require("./config.json");
 
 const totalFiles = new Collection();
 
@@ -7,16 +8,33 @@ const checkDir = (dir) => {
 
     Fs.readdir(dir, (error, files) => files.forEach((file) => {
 
+        if (error) console.log(error);
+
+        if (Config.excludeExtensions.includes(file.split(".").length > 1 ? file.split(".").pop().toLowerCase() : "dir"))
+            return;
+
         Fs.lstat(dir + "/" + file, (error, stat) => {
+
+            if (error) console.log(error);
 
             if (stat.isDirectory()) checkDir(dir + "/" + file);
             else Fs.readFile(dir + "/" + file, (error, buffer) => {
+
+                if (error) console.log(error);
+
                 const copy = totalFiles.findKey((val) => val.equals(buffer));
-                if (copy) console.log(dir + "/" + file + " is a copy of " + copy);
-                totalFiles.set(dir + "/" + file, buffer);
+                if (copy) {
+                    console.log(dir + "/" + file + " is a copy of " + copy);
+                    if (Config.delete) {
+                        Fs.unlink(dir + "/" + file, (error) => {
+                            if (error) console.log(error);
+                        });
+                    }
+                } else
+                    totalFiles.set(dir + "/" + file, buffer);
             });
         });
     }));
 }
 
-checkDir(__dirname + "/test-files");
+checkDir(Config.path);
