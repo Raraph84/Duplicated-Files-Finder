ce9d9b319b702af6c270a4a82bfb160a1f99df20
Raraph84 <contact@raraph.fr>
2024-06-24 17:49:08 +0200

    Add excludeDirs to config ; Fix crash with permissions ; Fix crash with big files

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-const { promises: fs } = require("fs");
+const { promises: fs, createReadStream } = require("fs");
+const { join, resolve, normalize } = require("path");
 const { createHash } = require("crypto");
-const Config = require("./config.json");
+const config = require("./config.json");
 
 const totalFiles = [];
 
 const checkDir = async (dir) => {
 
+    if (config.excludeDirs.some((excludeDir) => dir.endsWith(normalize(excludeDir))))
+        return;
+
     const files = await fs.readdir(dir);
 
     for (const file of files) {
 
-        if (Config.excludeExtensions.includes(file.split(".").length > 1 ? file.split(".").pop().toLowerCase() : "dir"))
+        if (config.excludeExtensions.includes(file.split(".").length > 1 ? file.split(".").pop().toLowerCase() : "dir"))
             continue;
 
-        const stat = await fs.lstat(dir + "/" + file);
+        const filePath = join(dir, file);
 
-        if (stat.isDirectory()) {
-            await checkDir(dir + "/" + file);
+        let stat;
+        try {
+            stat = await fs.lstat(filePath);
+        } catch (error) {
+            console.log("Cannot access " + filePath + " - " + error);
             continue;
         }
 
-        const buffer = await fs.readFile(dir + "/" + file);
-        const hash = createHash("md5").update(buffer).digest("hex");
+        if (stat.isDirectory()) {
+            await checkDir(filePath);
+            continue;
+        }
+
+        const hash = await new Promise((resolve, reject) => {
+            const hash = createHash("md5");
+            const stream = createReadStream(filePath);
+            stream.on("data", (data) => hash.update(data));
+            stream.on("end", () => resolve(hash.digest("hex")));
+            stream.on("error", reject);
+        });
 
         const copy = totalFiles.find((file) => file.hash === hash);
         if (copy) {
-            console.log(dir + "/" + file + " is a copy of " + copy.path);
-            if (Config.delete) await fs.unlink(dir + "/" + file);
+            console.log(filePath + " is a copy of " + copy.path);
+            if (config.delete) await fs.unlink(filePath);
         } else
-            totalFiles.push({ path: dir + "/" + file, hash });
+            totalFiles.push({ path: filePath, hash });
     }
 }
 
-checkDir(Config.path);
+checkDir(resolve(config.path));
